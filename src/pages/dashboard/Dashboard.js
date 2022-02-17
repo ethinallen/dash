2b57b5d558c93e853a7d7d2e75afae04be94ebc6
Drew <abemery@clemson.edu>
2022-02-17 11:23:22 -0500

    remove media for now :/

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -22,21 +22,23 @@ import BigStat from "./components/BigStat/BigStat";
 export default function Dashboard(props) {
   var classes = useStyles();
   var theme = useTheme();
-  console.log(data);
+
   return (
     <>
       <PageTitle title="Home" />
         <Grid container spacing={1}>
-          <Grid item xs={6} align="center" justify="center" alignItems="center"  >
-            <img className="photo" style={{"height" : "60vh", "display" : "flex", "align-itmes" : "right", "border-radius" : "4px"}} src="https://cdn.andrewemery.io/IMG_5273.jpg"></img>
-          </Grid>
-          {data.windows.map((windowData, index) =>
-            <Grid item xs={windowData.size}>
-              <Window
-              windowData={windowData}
-              />
-            </Grid>
-          )}
+            <Window windowData={{
+              "size"    : 12,
+              "header"  : "hello! my name is drew",
+              "body" : [
+                      "i love meeting people, especially you!",
+                      "to be honest, i have no idea why you are here.. but i would appreciate getting to know what brought you here",
+                      "for work i am a solutions engineer @Auth0 (now @Okta)",
+                      "if you feel so inclined, please reach out to me at: drew@emery.guru",
+                      ]
+            }} >
+              <img className="photo" style={{"height" : "60vh", "display" : "flex", "align-itmes" : "right", "border-radius" : "4px"}} src="https://cdn.andrewemery.io/IMG_5273.jpg"></img>
+          </Window>
         </Grid>
     </>
   );
